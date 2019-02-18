de3166fcb1b35218792d9b53db0c131c30f6fe8d
Yu Bai <yub@stanford.edu>
2019-02-18 14:58:42 -0800

    Adding scripts

---
+++ scripts/train-cifar10-binaryconnect.sh
@@ -0,0 +1,11 @@
+# Train binarized ResNets on CIFAR-10 with BinaryConnect
+LR=0.01
+DEPTH=20
+
+for i in 0 1 2 3
+do
+    python ../main_binary_reg.py --model resnet --resume results/resnet"$DEPTH" --model_config "{'depth': $DEPTH}" --save resnet"$DEPTH"_bc_Adam_run_$i --dataset cifar10 --gpu $i --batch-size 128 --epochs 300 --binary_reg 1.0 --tb_dir tb/resnet"$DEPTH"_bc_Adam_Freeze_200_run_$i --optimizer Adam --lr $LR --binary_regime --projection_mode lazy --freeze_epoch 200 &
+done
+
+wait
+echo all processes complete
---
+++ scripts/train-cifar10-pq-binary.sh
@@ -0,0 +1,11 @@
+# Train binarized ResNets on CIFAR-10
+LR=0.01
+DEPTH=20
+
+for i in 0 1 2 3
+do
+    python ../main_binary_reg.py --model resnet --resume results/resnet"$DEPTH" --model_config "{'depth': $DEPTH}" --save resnet"$DEPTH"_prox_Adam_run_$i --dataset cifar10 --gpu $i --batch-size 128 --epochs 300 --reg_rate 1e-4 --tb_dir tb/resnet"$DEPTH"_prox_Adam_Freeze_200_run_$i --optimizer Adam --lr $LR --projection_mode prox --freeze_epoch 200 &
+done
+
+wait
+echo all processes complete
